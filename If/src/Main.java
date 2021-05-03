b18c1e89bc04942e54ccaf3e9c11880e1a6a9a24
kosuke matsui <k.matsui0@icloud.com>
2021-05-03 14:25:32 +0900

    push

---
+++ If/src/Main.java
@@ -0,0 +1,132 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+//		※課題ソースコードのコメントアウトを外して取り組みましょう
+//
+
+
+		{
+//			◻️問１ if文の条件にtrueを記載して中身を実行する。（コードのコメントアウトを外してください）
+//			if(ここに記載) {
+//				System.out.println("条件がtrueなので実行します");
+//			}
+		}
+
+
+
+		{
+//			◻️問２ if文の条件にfalseを記載して実行する。（コードのコメントアウトを外してください）
+//			if(ここに記載) {
+//				System.out.println("条件がfalseなので実行されません");
+//			}
+		}
+
+
+
+		{
+//			◻️問３ 問２のコードの後ろにelse文を記載しましょう。（else文の中身はなんでも良し）
+//			if(false) {
+//				System.out.println("条件がfalseなので実行されません");
+//			}
+		}
+
+
+
+		{
+//			◻️問４ ・1==2
+//			     ・1!=2
+//			     ・a==b
+//			     ・a!=b
+//			     ・false||true
+//			     ・1==2||1==1
+//			     ・true&&false
+//			     上記の7つをそれぞれコンソール画面に出力するとなんて表示されるでしょうか？？（trueかfalseです）
+			int a = 100;
+			int b = 101;
+
+		}
+
+
+
+
+		{
+//			◻️問５ Aが出力されるように条件を記載してください。
+//			（コードのコメントアウトを外してください）
+//			（条件はなんでもオーケー）
+
+//			if() {
+//				System.out.println("B");
+//			}else if() {
+//				System.out.println("A");
+//			}
+
+		}
+
+
+
+
+		{
+//			◻️問６ "C"が出力されるように条件を記載してください。
+//			（コードのコメントアウトを外してください）
+//			（条件はなんでもオーケー）
+
+//			if() {
+//				System.out.println("A");
+//			}else{
+//				if() {
+//					System.out.println("B");
+//				}else {
+//					System.out.println("C");
+//				}
+//			}
+
+		}
+
+
+
+		{
+//			◻️問７ nameとaddressがそれぞれ正しければ"A"と出力します。
+//			      （文字列の比較には==ではなく、equalsメソッドを使用します。）
+			String name = "Aさん";
+			String address = "288769";
+
+//			if(name.equals(ここに記載) && address.equals(ここに記載)) {
+//				System.out.println("A");
+//			}
+		}
+
+
+
+
+
+		{
+//			◻️問８ nameとaddressがどちらかが正しければ"A"と出力します。（コードのコメントアウトを外してください）
+			String name = "Aさん";
+			String address = "288769";
+
+//			if(name.equals("Bさん") ここに記載 address.equals("288769")) {
+//				System.out.println("A");
+//			}
+		}
+
+
+		{
+//			◻️問９ AとBがどちらも7であれば、"AとBが7"と出力します。
+//			       Aだけが7の場合は、"Aだけ7"と出力します。
+//			       Bだけが7の場合は、"Bだけ7"と出力します。（コードのコメントアウトを外してください）
+			int A = 7;
+			int B = 7;
+
+//			if( && ) {
+//				System.out.println("AとBが7");
+//			}else if(){
+//			    System.out.println("Aだけ7");
+//			}else if(){
+//			    System.out.println("Bだけ7");
+//			}
+		}
+
+	}
+
+}
