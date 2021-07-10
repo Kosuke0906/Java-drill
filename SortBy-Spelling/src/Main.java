08cbb38e897bf10134cdbd074e34abc593d02237
kosuke matsui <k.matsui0@icloud.com>
2021-07-10 12:35:33 +0900

    push

---
+++ SortBy-Spelling/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+//		Question 入力者よりローマ字で値が入力されます。
+//		形式は一限配列です。（例：String[] array = ["John","hony", "Strength"]）
+//		頭文字を認識し配列をABC順に並び替えて出力してください。
+	}
+
+}
