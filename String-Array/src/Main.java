f57812d55fa1c4d08d1156999e98192b403fcb7d
kosuke matsui <k.matsui0@icloud.com>
2021-07-10 11:55:36 +0900

    push

---
+++ String-Array/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+//		Question 任意の文字列が入力されます。数字とそれ以外を分割するプログラムを作成しましょう。
+//		数字とそれ以外を配列に分けて出力してください。
+
+	}
+
+}
