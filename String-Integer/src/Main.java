eebf74b51081bbc0b77736637ac44da443e6043b
kosuke matsui <k.matsui0@icloud.com>
2021-07-10 12:09:42 +0900

    push

--- String-Integer/src/Main.java
+++ String-Integer/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-//		Question 任意の文字列が入力されます。数字とそれ以外を分割するプログラムを作成しましょう。
+//		Question 任意の文字列が入力されます。数字(Iint型とInteger型)とそれ以外を分割するプログラムを作成しましょう。
 //		数字とそれ以外を配列に分けて出力してください。
 
 	}
