6b431af1fb71a67466de37584ed5b77d360ab909
kosuke matsui <k.matsui0@icloud.com>
2021-07-18 01:56:31 +0900

    add new

---
+++ totall-Sabunn/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+//		問 以下の配列から２つの数字を１組として全ての場合を取得した後、
+//		　　それぞれの差分を全て足した合計を出力して下さい。
+		int array[] = {1, 5, 21, 9, 14, 41, 35};
+	}
+
+}
