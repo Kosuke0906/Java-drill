2ebcecfb805804001ab68b8e9e488a9f2ad2570e
kosuke matsui <k.matsui0@icloud.com>
2021-07-23 19:05:41 +0900

    push

--- Get-PrimeNumber/src/Main.java
+++ Get-PrimeNumber/src/Main.java
@@ -1,10 +1,10 @@
 
 public class Main {
-	public static int array[] = {3,7,45,1,6,75,85,24,11,87,43};
+	public static int array[] = {3,7,45,1,6,75,85,24,11,87};
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 //		Question 素数を抽出するプログラムを作成しましょう。パラメータはint型の配列で入力されます。
-//		要素数は入力者の任意です。
+//		要素数は10。
 
 	}
 }
---
+++ Teat/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+public static void main(String[] args) {
+//	Question 整数Aが書かれたカードを持っています。
+//	　　　　　1~Aの中で2で割れる回数が、一番多い数値を出力しなさい。
+//	　　　　　例）A=12
+//	         A/2 =6, 6/2=3, 二回
+	int A=68;
+	}
+}
