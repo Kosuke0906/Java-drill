1ea59ffb8fd2736b1e2b7e56dd9e4faabd3a063a
*kMVMQ04WHg <k-matsui@y-i-group.co.jp>
2021-11-21 03:16:50 +0900

    問題修正

--- MaxNumber-Iteration/src/Main.java
+++ MaxNumber-Iteration/src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
 public static void main(String[] args) {
-//	Question 整数Aが書かれたカードを持っています。
-//	　　　　　1~Aの中で2で割れる回数が、一番多い数値を出力しなさい。
-//	　　　　　例）A=12
-//	         A/2 =6, 6/2=3, 二回
-	int A=68;
+//	Question 1~100で偶数だけ取り出し、それぞれ何回２で割れるかを出力してください。
+
 	}
 }
