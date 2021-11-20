e40b82b581bd01376647de9af1d6ba4067f4b494
*kMVMQ04WHg <k-matsui@y-i-group.co.jp>
2021-11-21 03:28:14 +0900

    問題修正

--- answer/MaxNumber-Iteration/src/Main.java
+++ answer/MaxNumber-Iteration/src/Main.java
@@ -1,6 +1,18 @@
+
 public class Main {
 public static void main(String[] args) {
 //	Question 1~100で偶数だけ取り出し、それぞれ何回２で割れるかを出力してください。
-
+	int A=68;
+	for(int i = 1;i<=A;i++) {
+		if(i%2==0){
+			int num=i;
+			int count = 0;
+			while(num%2==0) {
+				num=num/2;
+				count++;
+			}
+			System.out.println(i+"は2で"+count+"回割れます");
+		}
+	}
 	}
 }
